535b78ef039c236967b3929a05fc84b8f25952fd
Lamont Granquist <lamont@scriptkiddie.org>
2017-06-15 12:07:54 -0700

    document some horrible lolwut
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/berkshelf/api_client/connection.rb
+++ lib/berkshelf/api_client/connection.rb
@@ -26,7 +26,11 @@ module Berkshelf::APIClient
     # @option options [Float] :retry_interval (0.5)
     #   how long to wait (in seconds) between each retry
     def initialize(url, options = {})
-      @url = url
+      # it looks like Faraday mutates the URI argument it is given, when we ripped Faraday out of this
+      # API it stopped doing that.  this may or may not be a breaking change (it broke some fairly
+      # brittle berkshelf tests).  if it causes too much berkshelf chaos we could revert by uncommenting
+      # the next line.  as it is removing this behavior feels more like fixing a bug.
+      #@url = url.normalize! if url.is_a?(Addressable::URI)
       options         = {retries: 3, retry_interval: 0.5, open_timeout: 30, timeout: 30}.merge(options)
       options[:server_url] = url
       # FIXME: translate from ApiClient->Ridley->Chef::HTTP
